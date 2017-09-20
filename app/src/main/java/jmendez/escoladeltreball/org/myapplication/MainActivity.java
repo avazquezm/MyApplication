bc16ef1cf36adf91e32c760b2aa409c7b5fc4202
sacabornero <sheldon1990>
2017-09-20 20:07:33 +0200

    Button added with an event.

--- app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
+++ app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package jmendez.escoladeltreball.org.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,4 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void btn_clickOnClick(View view){
+        Toast.makeText(this,"Button clicked!",Toast.LENGTH_LONG).show();
+
+
+    }
+
+
 }
