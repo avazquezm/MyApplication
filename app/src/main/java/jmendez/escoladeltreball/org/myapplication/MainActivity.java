e5c7ea74e4c38047faad81aa1b339c53237b584d
sacabornero <sheldon1990>
2017-09-20 18:26:57 +0200

    No changes

--- app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
+++ app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     Soy un cutre
     Otra vez
     */
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
